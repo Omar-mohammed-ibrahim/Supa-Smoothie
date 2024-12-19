af807731971a2768af003306e9965114580cf187
Omar <omarmohammed0124@gamil.com>
2024-12-19 16:40:38 +0200

    import Upload page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import Create from "./pages/Create"
 import Update from "./pages/Update"
+import Upload from "./pages/Upload"
 
 
 function App() {
